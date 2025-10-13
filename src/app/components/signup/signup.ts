3a48fc2aa450876a0ddfc0def238dd77aee89414
vkoech <koech36@gmail.com>
2025-10-13 17:35:01 +0300

    changes

--- src/app/components/signup/signup.ts
+++ src/app/components/signup/signup.ts
@@ -21,7 +21,7 @@ export class Signup {
     private router: Router,
      private notificationService: NotificationService){
      
-      this.getGeoLocation()
+      // this.getGeoLocation()
 
      this.SignUpForm = this.fb.group({
         emailAddress: ['', Validators.required],
